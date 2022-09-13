08641d1bed6c90529afee196384622616a4478b1
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2022-09-13 14:22:52 -0300

    main page finish

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg'
 
 function App() {
   return (
-    <div className='max-h-[1344px] mx-auto flex flex-col items-center my-20'>
+    <div className='max-w-[1300px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} />
 
       <h1 className='text-6xl text-white font-black mt-20'>
@@ -61,6 +63,22 @@ function App() {
           </div>
         </a>
       </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2632] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 text-white hover:bg-violet-600 rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
